71d0d7b9756d3843438f97db048a68297a86cdd9
zeng <zxx457@outlook.com>
2022-11-01 21:37:33 +0800

    2022-11-1

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,1 @@
-type MyOmit<T, K> = any
+type MyOmit<T, K> = Pick<T, Exclude<keyof T, K>>
--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,3 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [index in K]: T[index]
+}
