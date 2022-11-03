5153327feb3b0ccc407b28f2543d1d3e379482fa
zeng <zxx457@outlook.com>
2022-11-03 22:44:49 +0800

    2022-11-3

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,5 @@
-type Length<T> = any
+type Length<T extends ReadonlyArray<any>> = T extends [] ? 0 : T["length"]
+
+type LengthV2<T> = T extends { length: infer K } ? K : never
+
+type LengthV3<T extends readonly any[]> = T["length"]
