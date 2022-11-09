00589f4eb5d846f952d3ab13814ca85994415839
zeng <zxx457@outlook.com>
2022-11-09 20:19:58 +0800

    deep readonly

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,25 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+  readonly [Key in keyof T]: T[Key] extends
+    | Record<string, unknown>
+    | Array<unknown>
+    ? DeepReadonly<T[Key]>
+    : T[Key]
+}
+
+type DeepReadOnly2<T> = {
+  readonly [Key in keyof T]: T[Key] extends infer U
+    ? U extends (...args: any) => any
+      ? U
+      : DeepReadonly<U>
+    : never
+}
+
+type Text = {
+  x: string
+  y: string
+  z: {
+    p: string
+  }
+}
+
+type W = DeepReadonly<Text>
